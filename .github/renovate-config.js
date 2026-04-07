d1a66d8fb050a4091f03cfea01cec6a559a894fb
Kimonas Sotirchos <kimonas.sotirchos@mistral.ai>
2026-04-07 17:42:58 +0200

    specify repo

--- .github/renovate-config.js
+++ .github/renovate-config.js
@@ -1,9 +1,15 @@
 module.exports = {
+  // Global settings
+  endpoint: "https://api.github.com/",
+  repositories: ["kimwnasptd/helm-testing"],
+
   allowScripts: true,
   allowedCommands: [
     "^make update-date$",
     "^uv run python updater.py$"
   ],
+
+  // PR Settings
   packageRules: [
     {
       matchManagers: ["helmv3"],
